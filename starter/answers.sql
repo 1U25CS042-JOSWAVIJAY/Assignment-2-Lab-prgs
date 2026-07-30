75c5e26d40f67af21771a32f9ea2c1eac48d3138
prabhak-rvs <prabhak@rvsgroup.com>
2026-07-30 17:37:24 +0530

    Create answers.sql

---
+++ starter/answers.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE Student (
+    StudentID NUMBER(5) PRIMARY KEY,
+    StudentName VARCHAR2(20) NOT NULL UNIQUE,
+    DOB DATE NOT NULL,
+    Gender VARCHAR2(10) NOT NULL,
+    DepartmentID NUMBER(5) NOT NULL
+);
